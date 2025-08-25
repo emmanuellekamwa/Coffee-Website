06ccf10d69c7dc0dab503ff282b99e106006ccce
Emmanuelle Kamwa <emmanuellekamwa@gmail.com>
2025-08-25 21:40:27 +0100

    Styled swiper functionality

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ menuOpenButton.addEventListener("click", () => {
 menuCloseButton.addEventListener("click", () => {
   document.body.classList.remove("show-mobile-menu");
 });
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1, 
+    },
+    768: {
+      slidesPerView: 2, 
+    },
+    1024: {
+      slidesPerView: 3, 
+    }
+  }
+});
